859ffa747a1e1cfb1a529c5c756abb4f56f9e589
eilocarinas <locarinasangeline@gmail.com>
2023-02-16 09:19:27 +0800

    fourier_lowpass

--- tracks.py
+++ tracks.py
@@ -1,67 +1,68 @@
 from notes import *
 
-part1= [G1, Gb1_eight, A1_sixth, G1, #Ba-yang Ma-gi-
-                   D1, A1_eight, B1_eight, C2_eight,  #-liw per-las ng
-                   B1_eight, A1_sixth, B1, G1_eight, rest_eight, #si-la-nga-nan
-                   G1, Gb1_eight, A1_sixth,  G1,  #a-lab-ng-pu-
-                   D1,A1_eight, B1_eight, C2_eight, #-so-sa-dib-dib-
-                   B1_eight, A1_sixth, G1, rest,
-                   G1, Gb1_eight, A1_sixth, G1, #Ba-yang Ma-gi-
-                   D1, A1_eight, B1_eight, C2_eight,  #-liw per-las ng
-                   B1_eight, A1_sixth, B1, G1_eight, rest_eight, #si-la-nga-nan
-                   G1, Gb1_eight, A1_sixth,  G1,  #a-lab-ng-pu-
-                   D1,A1_eight, B1_eight, C2_eight, #-so-sa-dib-dib-
-                   B1_eight, A1_sixth, G1, rest_eight] #-mo'y bu -hay
 
-part1_filler= [G1_long, Gb1_long, A1_long, G1_long, 
-                D1_long, A1_long, B1_long, C2_long, 
-                B1_long, A1_long, B1_long, G1_long, rest_eight,
-                G1_long, Gb1_long, A1_long,  G1_long, 
-                D1_long, A1_long, B1_long, C2_long, 
-                B1_long, A1_long, G1_long, rest,
-                G1_long, Gb1_long, A1_long, G1_long, 
-                D1_long, A1_long, B1_long, C2_long, 
-                B1_long, A1_long, B1_long, G1_long, rest_eight,
-                G1_long, Gb1_long, A1_long,  G1_long, 
-                D1_long, A1_long, B1_long, C2_long, 
-                B1_long, A1_long, G1_long, rest_eight]
+part1= [G4, Gb4_eight, A4_sixth, G4, #Ba-yang Ma-gi-
+                   D4, A4_eight, B4_eight, C5_eight,  #-liw per-las ng
+                   B4_eight, A4_sixth, B4, G4_eight, rest_eight, #si-la-nga-nan
+                   G4, Gb4_eight, A4_sixth,  G4,  #a-lab-ng-pu-
+                   D4,A4_eight, B4_eight, C5_eight, #-so-sa-dib-dib-
+                   B4_eight, A4_sixth, G4, rest,
+                   G4, Gb4_eight, A4_sixth, G4, #Ba-yang Ma-gi-
+                   D4, A4_eight, B4_eight, C5_eight,  #-liw per-las ng
+                   B4_eight, A4_sixth, B4, G4_eight, rest_eight, #si-la-nga-nan
+                   G4, Gb4_eight, A4_sixth,  G4,  #a-lab-ng-pu-
+                   D4,A4_eight, B4_eight, C5_eight, #-so-sa-dib-dib-
+                   B4_eight, A4_sixth, G4, rest_eight] #-mo'y bu -hay
+
+part1_filler= [G4_long, Gb4_long, A4_long, G4_long, 
+                D4_long, A4_long, B4_long, C5_long, 
+                B4_long, A4_long, B4_long, G4_long, rest_eight,
+                G4_long, Gb4_long, A4_long,  G4_long, 
+                D4_long, A4_long, B4_long, C5_long, 
+                B4_long, A4_long, G4_long, rest,
+                G4_long, Gb4_long, A4_long, G4_long, 
+                D4_long, A4_long, B4_long, C5_long, 
+                B4_long, A4_long, B4_long, G4_long, rest_eight,
+                G4_long, Gb4_long, A4_long,  G4_long, 
+                D4_long, A4_long, B4_long, C5_long, 
+                B4_long, A4_long, G4_long, rest_eight]
 
 
-part2 = [G1_eight, Gb1_sixth, G1_eight, A1_eight, 
-         A1_sixth, D1_eight, D1_sixth, A1_eight, 
-         A1_sixth, D1_eight, D1_sixth,
-         B1_eight, C2_sixth, D2_eight, E2_sixth, D2,
-         G1_eight, Gb1_sixth, G1_eight, A1_eight, A1_sixth, D1_eight, D1_sixth, A1_eight, A1_sixth, D1_eight, D1_sixth,
-         B1_sixth, C2_sixth, B1_sixth, A1_sixth, B1_sixth, A1_sixth, G1] 
+part2 = [G4_eight, Gb4_sixth, G4_eight, A4_eight, 
+         A4_sixth, D4_eight, D4_sixth, A4_eight, 
+         A4_sixth, D4_eight, D4_sixth,
+         B4_eight, C5_sixth, D5_eight, E5_sixth, D5,
+         G4_eight, Gb4_sixth, G4_eight, A4_eight, A4_sixth, D4_eight, D4_sixth, A4_eight, A4_sixth, D4_eight, D4_sixth,
+         B4_sixth, C5_sixth, B4_sixth, A4_sixth, B4_sixth, A4_sixth, G4] 
 
-part2_filler = [G1_long, Gb1_long, G1_eight, A1_long, A1_long, D1_long, D1_long, A1_long, A1_long, D1_long, D1_long,
-                B1_long, C2_long, D2_long, E2_long, D2_long,
-                G1_long, Gb1_long, G1_long, A1_long, A1_long, D1_long, D1_long, A1_long, A1_long, D1_long, D1_long,
-                B1_long, C2_long, B1_long, A1_long, B1_long, A1_long, G1_long] 
+part2_filler = [G4_long, Gb4_long, G4_eight, A4_long, A4_long, D4_long, D4_long, A4_long, A4_long, D4_long, D4_long,
+                B4_long, C5_long, D5_long, E5_long, D5_long,
+                G4_long, Gb4_long, G4_long, A4_long, A4_long, D4_long, D4_long, A4_long, A4_long, D4_long, D4_long,
+                B4_long, C5_long, B4_long, A4_long, B4_long, A4_long, G4_long] 
 
-part3 = [G1_eight, E1_sixth, G1_eight, C2, C2,    #Lu-pa ng a-raw
-             D3, D3, E1_eight, D2_sixth, C2_eight, D2_sixth, E1,    #ng luwal-ha-ti't pag-sin-ta
-             G1_eight, E1_eight, D2_eight, E1, C2, D2, B1_eight, B1_eight, C2, rest    #Bu-hay ay la-ngit sa pi-ling mo
+part3 = [G4_eight, E4_sixth, G4_eight, C5, C5,    #Lu-pa ng a-raw
+             D6, D6, E4_eight, D5_sixth, C5_eight, D5_sixth, E4,    #ng luwal-ha-ti't pag-sin-ta
+             G4_eight, E4_eight, D5_eight, E4, C5, D5, B4_eight, B4_eight, C5, rest    #Bu-hay ay la-ngit sa pi-ling mo
              ]
 
-part3_filler = [G1_long, E1_long, G1_long, C2_long, C2_long, #Lu-pa ng a-raw
-                    D2_long, D2_long, E1_long, D2_long, C2_long, D2_long, E1_long, #ng luwal-ha-ti't pag-sin-ta
-                    F1_long, E1_long, D2_long, E1_long, C2_long, D2_long, B1_long, B1_long, C2_long, rest #Bu-hay ay la-ngit sa pi-ling mo
+part3_filler = [G4_long, E4_long, G4_long, C5_long, C5_long, #Lu-pa ng a-raw
+                    D5_long, D5_long, E4_long, D5_long, C5_long, D5_long, E4_long, #ng luwal-ha-ti't pag-sin-ta
+                    F4_long, E4_long, D5_long, E4_long, C5_long, D5_long, B4_long, B4_long, C5_long, rest #Bu-hay ay la-ngit sa pi-ling mo
                     ]
 
-part4 = [G1_eight, E1_sixth, G1_eight, C2,
-          C2, D2, D2, E1_eight, 
-          D2_sixth, C2_eight, D2_sixth, E1, 
-          Gb1_eight, E1_eight, D2_eight, E1, 
-          C2, D2, B1_eight, B1_sixth, C2, rest
+part4 = [G4_eight, E4_sixth, G4_eight, C5,
+          C5, D5, D5, E4_eight, 
+          D5_sixth, C5_eight, D5_sixth, E4, 
+          Gb4_eight, E4_eight, D5_eight, E4, 
+          C5, D5, B4_eight, B4_sixth, C5, rest
           ] 
 
-part4_filler = [G1_long, E1_long, G1_long, C2_long, 
-                    C2_long, D2_long, D2_long, E1_long, 
-                    D2_long, C2_long, D2_long, E1_long,
-          	        Gb1_long, E1_long, D2_long, E1_long, 
-                    C2_long, D2_long, B1_long, B1_long, 
-                    C2_long, rest
+part4_filler = [G4_long, E4_long, G4_long, C5_long, 
+                    C5_long, D5_long, D5_long, E4_long, 
+                    D5_long, C5_long, D5_long, E4_long,
+          	        Gb4_long, E4_long, D5_long, E4_long, 
+                    C5_long, D5_long, B4_long, B4_long, 
+                    C5_long, rest
                    ] 
 
 def createSpace(track, attack=100, release=100):
@@ -74,6 +75,7 @@ def createSpace(track, attack=100, release=100):
 note1 = []
 note2 = []
 
+#creates function to mix two tracks
 def mix2tracks(track1, track2):
     createSpace(track1, attack=50, release=50)
     createSpace(track2, attack=50, release=50)
@@ -83,6 +85,7 @@ def mix2tracks(track1, track2):
         note2 = track2[i]
         song += note1[:len(note1)].overlay(note2[:len(note2)])
     return song
+#mixing two tracks
 
 part1 = mix2tracks(track1=part1, track2=part1_filler)
 part2 = mix2tracks(track1=part2, track2=part2_filler)
